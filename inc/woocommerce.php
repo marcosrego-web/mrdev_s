ab70ea6bdfbb6814a77de923936ae96cb79d0bd7
Marcos Rego <web@marcosrego.com>
2021-04-24 00:29:18 +0100

    Remove woocommerce wrapper made by Underscores

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -106,38 +106,6 @@ add_filter( 'woocommerce_output_related_products_args', '_s_woocommerce_related_
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
 
-if ( ! function_exists( '_s_woocommerce_wrapper_before' ) ) {
-	/**
-	 * Before Content.
-	 *
-	 * Wraps all WooCommerce content in wrappers which match the theme markup.
-	 *
-	 * @return void
-	 */
-	function _s_woocommerce_wrapper_before() {
-		?>
-			<main id="primary" class="mr-main site-main">
-		<?php
-	}
-}
-add_action( 'woocommerce_before_main_content', '_s_woocommerce_wrapper_before' );
-
-if ( ! function_exists( '_s_woocommerce_wrapper_after' ) ) {
-	/**
-	 * After Content.
-	 *
-	 * Closes the wrapping divs.
-	 *
-	 * @return void
-	 */
-	function _s_woocommerce_wrapper_after() {
-		?>
-			</main><!-- #main -->
-		<?php
-	}
-}
-add_action( 'woocommerce_after_main_content', '_s_woocommerce_wrapper_after' );
-
 /**
  * Sample implementation of the WooCommerce Mini Cart.
  *
