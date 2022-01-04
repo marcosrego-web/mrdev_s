a01383f0107709fd420cf49b3bb3314eb1cb7729
Marcos Rego <web@marcosrego.com>
2022-01-04 01:41:47 +0000

    Changed font styles to match Mr.Utils defaults

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  */
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '0.9.447' );
+	define( '_S_VERSION', '0.9.448' );
 }
 if ( ! function_exists( '_s_setup' ) ) :
 	/**
