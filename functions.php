0d8ab3a7e0e3671dfa48d2ff32c69b8b21e9c9f4
Marcos Rego <web@marcosrego.com>
2022-01-31 21:13:51 +0000

    Fixed var on admin styles + Gallery font-size WP5.9

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  */
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '0.9.454' );
+	define( '_S_VERSION', '0.9.455' );
 }
 if ( ! function_exists( '_s_setup' ) ) :
 	/**
@@ -123,5 +123,3 @@ if($mrdev_config_betatest === 1) {
 } else {
 	$myUpdateChecker->setBranch('master');
 }
-
-//add_action('after_setup_theme', 'mrdev_color_picker_blocks');
